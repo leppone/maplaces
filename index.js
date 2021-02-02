5ec4dd16348061ed4f587a9b198acf894bc7c702
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2021-02-02 21:30:40 +0200

    fix

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const pool = new Pool({
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/db', async (req, res) => {
     try {
