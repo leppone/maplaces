cebcef64f300f6dd44546bc0d1f8eea9c2edf42c
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2021-02-02 22:02:47 +0200

    testing old implementation

--- index.js
+++ index.js
@@ -1,132 +1,114 @@
 const express = require('express');
-const { Pool } = require('pg'); 
-const path = require('path');
-const PORT = process.env.PORT;
-const DB_URL = process.env.DATABASE_URL;
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+// Cors for enabling unmatching origins
+app.use(cors());
 
 
-const pool = new Pool({
-  connectionString: DB_URL,
-  ssl: {
-    rejectUnauthorized: false
+// Generate ID for item by scoreboard length
+const generateId = () => {
+  let scoreboard = db.get('scoreboard').value();
+  const maxId = scoreboard.length > 0
+    ? Math.max(...scoreboard.map(item => item.id))
+    : 0;
+  return maxId + 1;
+}
+
+// GET all items sorted by score
+app.get('/', (req, res) => {
+  // Sort results by score before returning
+  const hello = "hello world"
+  res.json(hello);
+})
+
+
+// GET all items sorted by score
+app.get('/api/scoreboard', (req, res) => {
+  console.log('GET /api/scoreboard called');
+  const scoreboard = db.get('scoreboard').value();
+
+  // Init scoreboard if empty
+  if( scoreboard === undefined) {
+    db.defaults({ scoreboard: [] });
   }
-});
+
+  // Sort results by score before returning
+  const sortedResults = scoreboard.sort((a, b) => b.score - a.score);
+  res.json(sortedResults);
+})
+
+// GET amount of items in scoreboard
+app.get('/api/scoreboard/size', (req, res) => {
+  const scoreboard = db.get('scoreboard').value();
+  res.json(scoreboard.length);
+})
 
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      console.log(DB_URL);
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM places_test');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-
-// // Generate ID for item by scoreboard length
-// const generateId = () => {
-//   let scoreboard = db.get('scoreboard').value();
-//   const maxId = scoreboard.length > 0
-//     ? Math.max(...scoreboard.map(item => item.id))
-//     : 0;
-//   return maxId + 1;
-// }
-
-// // GET all items sorted by score
-// app.get('/api/scoreboard', (req, res) => {
-//   console.log('GET /api/scoreboard called');
+// // GET streaming amount of items in scoreboard
+// app.get('/api/scoreboard/stream', (req, res) => {
+//   console.log('GET /api/scoreboard/stream called');
 //   const scoreboard = db.get('scoreboard').value();
+//   // Setting up event-stream response
+//   res.set({
+//     "Content-Type": "text/event-stream",
+//     "Cache-Control": "no-cache",
+//     Connection: "keep-alive",
 
-//   // Init scoreboard if empty
-//   if( scoreboard === undefined) {
-//     db.defaults({ scoreboard: [] });
-//   }
+//     // enabling CORS
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Headers":
+//     "Origin, X-Requested-With, Content-Type, Accept",  
+//   });
 
-//   // Sort results by score before returning
-//   const sortedResults = scoreboard.sort((a, b) => b.score - a.score);
-//   res.json(sortedResults);
-// })
+//   // Create eventInterval for streaming
+//   let eventInterval = setInterval(() => {
+//     res.write(`event: message\n`);
+//     res.write(`data: ${JSON.stringify(scoreboard)}\n\n`);
+//   }, 2000);
 
-// // GET amount of items in scoreboard
-// app.get('/api/scoreboard/size', (req, res) => {
-//   const scoreboard = db.get('scoreboard').value();
-//   res.json(scoreboard.length);
-// })
-
-
-// // // GET streaming amount of items in scoreboard
-// // app.get('/api/scoreboard/stream', (req, res) => {
-// //   console.log('GET /api/scoreboard/stream called');
-// //   const scoreboard = db.get('scoreboard').value();
-// //   // Setting up event-stream response
-// //   res.set({
-// //     "Content-Type": "text/event-stream",
-// //     "Cache-Control": "no-cache",
-// //     Connection: "keep-alive",
-
-// //     // enabling CORS
-// //     "Access-Control-Allow-Origin": "*",
-// //     "Access-Control-Allow-Headers":
-// //     "Origin, X-Requested-With, Content-Type, Accept",  
-// //   });
-
-// //   // Create eventInterval for streaming
-// //   let eventInterval = setInterval(() => {
-// //     res.write(`event: message\n`);
-// //     res.write(`data: ${JSON.stringify(scoreboard)}\n\n`);
-// //   }, 2000);
-
-// //   // Create eventInterval for streaming
-// //   req.on("close", (err) => {
-// //     clearInterval(eventInterval);
-// //     res.end(); 
-// //   });
-// // })
-
-
-// // POST item to database
-// app.post('/api/scoreboard', (req, res) => {
-//   console.log('POST /api/scoreboard called')
-//   const body = req.body;
-
-//   // Checking all required items exist
-//   if (!body.name || !body.score) {
-//     return response.status(400).json({ 
-//       error: 'Name or score missing'
-//     })
-//   }
-
-//   // Create item from request body
-//   const item = {
-//     name: body.name,
-//     score: body.score,
-//     id: generateId()
-//   }
-
-//   // Add item to "database"
-//   db.get('scoreboard')
-//     .push(item)
-//     .write();
-
-//   // Send ok response
-//   res.json(item);
+//   // Create eventInterval for streaming
+//   req.on("close", (err) => {
+//     clearInterval(eventInterval);
+//     res.end(); 
+//   });
 // })
 
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
+// POST item to database
+app.post('/api/scoreboard', (req, res) => {
+  console.log('POST /api/scoreboard called')
+  const body = req.body;
 
-// app.listen(port);
-// console.log(`Server running on port ${port}`);
+  // Checking all required items exist
+  if (!body.name || !body.score) {
+    return response.status(400).json({ 
+      error: 'Name or score missing'
+    })
+  }
+
+  // Create item from request body
+  const item = {
+    name: body.name,
+    score: body.score,
+    id: generateId()
+  }
+
+  // Add item to "database"
+  db.get('scoreboard')
+    .push(item)
+    .write();
+
+  // Send ok response
+  res.json(item);
+})
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port);
+console.log(`Server running on port ${port}`);
