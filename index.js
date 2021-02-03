9e69e15420ffc4abecf6379d417931de374b8589
Markus Lehtonen <markus.o.lehtonen@gmail.com>
2021-02-03 18:08:41 +0200

    get and post methods working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 const app = express();
 const PORT = process.env.PORT;
 const DB_URL = process.env.DATABASE_URL;
+const table_name = 'places_test';
 
 const pool = new Pool({
   connectionString: DB_URL,
@@ -28,7 +29,8 @@ const generateId = () => {
   return maxId + 1;
 }
 
-// GET all items sorted by score
+
+// GET index html page
 app.get('/', (req, res) => {
   // Sort results by score before returning
   const hello = "hello world"
@@ -36,99 +38,53 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/db', async (req, res) => {
+// GET all items
+app.get('/api/places', async (req, res) => {
+  console.log('GET /api/places called')
+
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM places_test');
+    const result = await client.query(`SELECT * FROM ${table_name}`);
     const results = { 'results': (result) ? result.rows : null};
     res.json(results);
     client.release();
-  } catch (err) {
+  } 
+  catch (err) {
     console.error(err);
-    res.send("Error " + err);
+    res.send(`Error ${err}`);
   }
 })
-
-
-// GET all items sorted by score
-app.get('/api/scoreboard', (req, res) => {
-  console.log('GET /api/scoreboard called');
-  const scoreboard = db.get('scoreboard').value();
-
-  // Init scoreboard if empty
-  if( scoreboard === undefined) {
-    db.defaults({ scoreboard: [] });
-  }
-
-  // Sort results by score before returning
-  const sortedResults = scoreboard.sort((a, b) => b.score - a.score);
-  res.json(sortedResults);
-})
-
-// GET amount of items in scoreboard
-app.get('/api/scoreboard/size', (req, res) => {
-  const scoreboard = db.get('scoreboard').value();
-  res.json(scoreboard.length);
-})
-
-
-// // GET streaming amount of items in scoreboard
-// app.get('/api/scoreboard/stream', (req, res) => {
-//   console.log('GET /api/scoreboard/stream called');
-//   const scoreboard = db.get('scoreboard').value();
-//   // Setting up event-stream response
-//   res.set({
-//     "Content-Type": "text/event-stream",
-//     "Cache-Control": "no-cache",
-//     Connection: "keep-alive",
-
-//     // enabling CORS
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Headers":
-//     "Origin, X-Requested-With, Content-Type, Accept",  
-//   });
-
-//   // Create eventInterval for streaming
-//   let eventInterval = setInterval(() => {
-//     res.write(`event: message\n`);
-//     res.write(`data: ${JSON.stringify(scoreboard)}\n\n`);
-//   }, 2000);
-
-//   // Create eventInterval for streaming
-//   req.on("close", (err) => {
-//     clearInterval(eventInterval);
-//     res.end(); 
-//   });
-// })
 
 
 // POST item to database
-app.post('/api/scoreboard', (req, res) => {
-  console.log('POST /api/scoreboard called')
-  const body = req.body;
+app.post('/api/places', async (req, res) => {
+  console.log('POST /api/places called')
 
-  // Checking all required items exist
-  if (!body.name || !body.score) {
-    return response.status(400).json({ 
-      error: 'Name or score missing'
-    })
+  // Checking all items from request
+  const {title, description, opening_hours, coordinates} = req.body;
+  if (!title) {
+    return res.status(400).json({ error: 'Title required' })
   }
 
-  // Create item from request body
-  const item = {
-    name: body.name,
-    score: body.score,
-    id: generateId()
+  // Try inserting data
+  try{
+    const client = await pool.connect();
+    client.query(
+      `INSERT INTO ${table_name} (title, description, opening_hours, coordinates) 
+      VALUES ( '${title}', '${description}', '${opening_hours}', '${coordinates}' )`,
+      (error) => {
+        if (error) { throw error; }
+        // If here, return ok
+        res.status(201).json({ status: 'success', message: 'Item added.' });
+      }
+    )
+    client.release();
   }
-
-  // Add item to "database"
-  db.get('scoreboard')
-    .push(item)
-    .write();
-
-  // Send ok response
-  res.json(item);
-})
+  catch (err) {
+    console.error(err);
+    res.send(`Error ${err}`);
+  }
+});
 
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
